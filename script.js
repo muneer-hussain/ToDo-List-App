802a3cb4bc0aeab015bb6aab80588e49412bbe0d
Muneer Hussain <97391190+muneer-hussain@users.noreply.github.com>
2022-06-10 23:54:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let textarea = document.querySelector("textarea")
 let addBtn = document.querySelector("#addTodo")
 let taskBox = document.getElementById("todos")
 let filters = document.querySelectorAll("#status span")
+let TasksLength = document.getElementById("tasks-length")
+
 
 
 let todos = JSON.parse(localStorage.getItem("todos") || "[]");
@@ -10,29 +12,62 @@ let todos = JSON.parse(localStorage.getItem("todos") || "[]");
 let isupdate = false,
     updateId;
 
+    filters.forEach(btn => {
+        btn.addEventListener("click", () => {
+            document.querySelector("span.active").classList.remove("active");
+            btn.classList.add("active");
+            showtodos(btn.id)
+        })
+    })
 
-function showtodos() {
+
+function showtodos(filters) {
     let todoHTML = ``;
-    todos.forEach(
-        (todo, index) => {
+    todos.forEach((todo, index) => {
+         let completed = todo.status == "completed" ? "checked" : "";
+        if (filters == todo.status || filters == "all") {    
             todoHTML += `
             <div class="todo-info-wrap">
                  <div class="todo-information">
                     <label for="${index}">
-                        <div class="todo-written"><input onclick="updateStatus(this)" type="checkbox" id="${index}">${todo.todoWritten}</div>
+                        <div class="todo-written">
+                        <input onclick="updateStatus(this, '${filters}')" type="checkbox" id="${index}" ${completed}>
+                        <span class="${completed}">${todo.todoWritten}</span>
+                        </div>
                     </label>
                 </div>
-                        <i class="fa fa-pencil tools" onclick="editTodo(${index}, '${todo.todoWritten}', '${todo.status}')"></i>
-                        <i class="fa fa-trash tools" onclick="deleteNote(${index})"></i>
+                        <i class="fa fa-pencil tools" onclick="editTodo(this, ${index}, '${todo.todoWritten}','${filters}')"></i>
+                        <i class="fa fa-trash tools" onclick="deleteNote(${index}, '${filters}')"></i>
                 </div>
              </div>
-            `
-        })
-    taskBox.innerHTML = todoHTML;
+            `   
+        }
+     })
+        taskBox.innerHTML = todoHTML;
+        TasksLength.innerHTML = taskLengthFunc() 
+
 
 }
 
-function editTodo(selectedId, written) {
+
+
+showtodos("all")
+
+
+function updateStatus(selectedTask, filter){
+    let taskName = selectedTask.parentElement;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todos", JSON.stringify(todos))
+    setTimeout(showtodos(filter), 200)    
+}
+
+function editTodo(selectedTsk, selectedId, written, filterEdt) {
     textarea.value = written;
     isupdate = true;
     updateId = selectedId;
@@ -40,38 +75,57 @@ function editTodo(selectedId, written) {
 }
 
 
-
-function deleteNote(selectedId) {
+function deleteNote(selectedId, filterdlt) {
     todos.splice(selectedId, 1)
     localStorage.setItem("todos", JSON.stringify(todos))
-    showtodos();
+    showtodos(filterdlt);
 }
 
-showtodos();
+function taskLengthFunc() {
+    todos.length
 
-function addTodo() {
+    if (todos.length < 2) {
+        return "Task = " + todos.length;
+    } else {
+        return "Tasks = " + todos.length;
+    }
+}
+
+
+function addTodo(e) {
+     let userTask = textareaTodo.value.trim();
     if (textareaTodo.value != 0) {
         let todoInfo = {
-            'todoWritten': textareaTodo.value.trim(),
+            'todoWritten': userTask,
+            'status': 'pending',
         }
 
         if (!isupdate) {
             todos.unshift(todoInfo)
         } else {
             isupdate = false;
-            todos[updateId] = todoInfo;
+            todos[updateId].todoWritten = userTask;
         }
         localStorage.setItem("todos", JSON.stringify(todos))
-        showtodos();
+        showtodos(document.querySelector("span.active"));
         textareaTodo.value = "";
     }
-}
 
+            textareaTodo.value = "";
+            localStorage.setItem("todos", JSON.stringify(todos));
+            showtodos(document.querySelector("span.active").id);
+
+
+}
+//          ToDo will add when user press enter key 
 addBtn.addEventListener("click", addTodo)
 document.addEventListener("keydown", (e) => {
     textarea.focus();
     if (e.keyCode == 13) {
         e.preventDefault();
         addTodo();
+
     }
 })
+
+
